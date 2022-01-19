e3a85f5657a000eef1c74b909666dfbcd5d22a38
username <nomadicalli@mail.ru>
2022-01-19 14:28:50 +0300

    release 1

--- tamagochi.js
+++ tamagochi.js
@@ -2,7 +2,7 @@ let tamagochi = {
     name: "Karl",
     food: 2,
     energy: 4,
-    mood: 5,
+    mood: 3,
 
     getStatus() {
         console.log(`Имя: ${this.name}, Еда: ${this.food}, Энергия: ${this.energy}, Настроение: ${this.mood}`);
@@ -10,5 +10,27 @@ let tamagochi = {
 
     setName(newName) {
         this.name = newName;
+    },
+
+    eat() {
+        if (this.food < 5) {
+            this.food ++;
+            this.mood --;
+        }
+    },
+
+    sleep() {
+        if (this.energy < 5) {
+            this.energy ++;
+            this.food --;
+        }
+    },
+
+    play() {
+        if (this.mood < 5) {
+            this.mood ++;
+            this.energy --;
+        }
     }
+
 }
